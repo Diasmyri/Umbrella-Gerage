bb858ea3f727cc1900b781d364d8a91be15542fc
Diasmyri <diasmayri15@gmail.com>
2025-10-31 18:48:24 +0700

    tambah services

---
+++ Services/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbrella_gerage.Services
+{
+    class Client
+    {
+    }
+}
