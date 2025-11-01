bc277bc2a87506e350a4b31af7c4d53d89aed691
Farhan-Almaki <Lathifatulalmaki@gmail.com>
2025-11-01 14:49:01 +0700

    Menambahkan logic di formdamaged

--- Forms/DamagedForm.Designer.cs
+++ Forms/DamagedForm.Designer.cs
@@ -28,12 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Damaged";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            txtPlateNumber = new TextBox();
+            txtDescription = new TextBox();
+            comboCarType = new ComboBox();
+            comboServiceType = new ComboBox();
+            dateReport = new DateTimePicker();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            label7 = new Label();
+            dataGridViewDamaged = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewDamaged).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(228, 34);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 20);
+            label1.TabIndex = 0;
+            label1.Text = "DAMAGED";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(27, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Plate Number";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(27, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Car Type";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(27, 176);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Service Type";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(27, 218);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Description";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(27, 255);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Report Date";
+            // 
+            // txtPlateNumber
+            // 
+            txtPlateNumber.Location = new Point(191, 84);
+            txtPlateNumber.Name = "txtPlateNumber";
+            txtPlateNumber.Size = new Size(155, 27);
+            txtPlateNumber.TabIndex = 6;
+            txtPlateNumber.TextChanged += textBox1_TextChanged;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(191, 214);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(155, 27);
+            txtDescription.TabIndex = 7;
+            txtDescription.TextChanged += txtDescription_TextChanged;
+            // 
+            // comboCarType
+            // 
+            comboCarType.FormattingEnabled = true;
+            comboCarType.Location = new Point(191, 127);
+            comboCarType.Name = "comboCarType";
+            comboCarType.Size = new Size(155, 28);
+            comboCarType.TabIndex = 8;
+            comboCarType.SelectedIndexChanged += comboCarType_SelectedIndexChanged;
+            // 
+            // comboServiceType
+            // 
+            comboServiceType.FormattingEnabled = true;
+            comboServiceType.Location = new Point(191, 172);
+            comboServiceType.Name = "comboServiceType";
+            comboServiceType.Size = new Size(155, 28);
+            comboServiceType.TabIndex = 9;
+            comboServiceType.SelectedIndexChanged += comboServiceType_SelectedIndexChanged;
+            // 
+            // dateReport
+            // 
+            dateReport.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateReport.Location = new Point(191, 255);
+            dateReport.Name = "dateReport";
+            dateReport.Size = new Size(284, 26);
+            dateReport.TabIndex = 10;
+            dateReport.ValueChanged += dateReport_ValueChanged;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdd.Location = new Point(119, 318);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(100, 23);
+            btnAdd.TabIndex = 11;
+            btnAdd.Text = "ADD";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnUpdate.Location = new Point(246, 318);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(100, 23);
+            btnUpdate.TabIndex = 12;
+            btnUpdate.Text = "UPDATE ";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDelete.Location = new Point(375, 318);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(100, 23);
+            btnDelete.TabIndex = 13;
+            btnDelete.Text = "DELETE";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(569, 34);
+            label7.Margin = new Padding(5, 0, 5, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(213, 20);
+            label7.TabIndex = 14;
+            label7.Text = "dataGridViewDamaged";
+            // 
+            // dataGridViewDamaged
+            // 
+            dataGridViewDamaged.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewDamaged.Location = new Point(514, 84);
+            dataGridViewDamaged.Name = "dataGridViewDamaged";
+            dataGridViewDamaged.Size = new Size(357, 257);
+            dataGridViewDamaged.TabIndex = 15;
+            dataGridViewDamaged.CellContentClick += dataGridViewDamaged_CellContentClick;
+            // 
+            // DamagedForm
+            // 
+            AutoScaleDimensions = new SizeF(11F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(912, 376);
+            Controls.Add(dataGridViewDamaged);
+            Controls.Add(label7);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(dateReport);
+            Controls.Add(comboServiceType);
+            Controls.Add(comboCarType);
+            Controls.Add(txtDescription);
+            Controls.Add(txtPlateNumber);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "DamagedForm";
+            Text = "Damaged";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewDamaged).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtPlateNumber;
+        private TextBox txtDescription;
+        private ComboBox comboCarType;
+        private ComboBox comboServiceType;
+        private DateTimePicker dateReport;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Label label7;
+        private DataGridView dataGridViewDamaged;
     }
 }
--- Forms/DamagedForm.cs
+++ Forms/DamagedForm.cs
@@ -5,16 +5,212 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;   
 using System.Windows.Forms;
+using Umbrella_gerage.Data;
+using Umbrella_gerage.Models;
+using Umbrella_gerage.Services;
 
 namespace Umbrella_gerage.Forms
 {
-    public partial class DamagedForm: Form
+    public partial class DamagedForm : Form
     {
+        private readonly DamagedService _damagedService;
         public DamagedForm()
         {
             InitializeComponent();
+            _damagedService = new DamagedService(new AppDbContext());
+        }
+        private void DamagedForm_Load(object sender, EventArgs e)
+        {
+            LoadServiceType();
+            LoadDamagedData();
+            dateReport.Value = DateTime.Now;
+        }
+        private void LoadServiceType()
+        {
+            // Daftar jenis servis
+            comboServiceType.Items.Clear();
+            comboServiceType.Items.AddRange(new string[]
+            {
+                "Ganti Oli",
+                "Servis Mesin",
+                "Ganti Ban",
+                "Cat Body",
+                "Perbaikan AC",
+                "Tune Up",
+                "Cuci Mobil"
+            });
+        }
+        private void LoadDamagedData()
+        {
+            try
+            {
+                var list = _damagedService.GetAll()
+                    .Select(d => new
+                    {
+                        d.PlateNumber,
+                        d.CarType,
+                        d.ServiceType,
+                        d.Description,
+                        d.ReportDate
+                    })
+                    .ToList();
+
+                dataGridViewDamaged.DataSource = list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+        }
+        private void ClearForm()
+        {
+            txtPlateNumber.Clear();
+            comboCarType.SelectedIndex=-1;
+            comboServiceType.SelectedIndex = -1;
+            txtDescription.Clear();
+            dateReport.Value = DateTime.Now;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboCarType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboServiceType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateReport_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtPlateNumber.Text) ||
+                    string.IsNullOrWhiteSpace(comboCarType.Text) ||
+                    comboServiceType.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Please fill in all required fields.");
+                    return;
+                }
+
+                var damaged = new Damaged
+                {
+                    PlateNumber = txtPlateNumber.Text.Trim(),
+                    CarType = comboCarType.Text.Trim(),
+                    ServiceType = comboServiceType.Text,
+                    Description = txtDescription.Text.Trim(),
+                    ReportDate = dateReport.Value
+                };
+
+                _damagedService.Add(damaged);
+                MessageBox.Show("Data added successfully!");
+
+                LoadDamagedData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding data: " + ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtPlateNumber.Text))
+                {
+                    MessageBox.Show("Please select a record to update.");
+                    return;
+                }
+
+                var damaged = new Damaged
+                {
+                    PlateNumber = txtPlateNumber.Text.Trim(),
+                    CarType = comboCarType.Text.Trim(),
+                    ServiceType = comboServiceType.Text,
+                    Description = txtDescription.Text.Trim(),
+                    ReportDate = dateReport.Value
+                };
+
+                _damagedService.Update(damaged);
+                MessageBox.Show("Data updated successfully!");
+
+                LoadDamagedData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating data: " + ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtPlateNumber.Text))
+                {
+                    MessageBox.Show("Please select a record to delete.");
+                    return;
+                }
+
+                var confirm = MessageBox.Show("Are you sure to delete this record?",
+                                              "Confirm Delete",
+                                              MessageBoxButtons.YesNo,
+                                              MessageBoxIcon.Warning);
+
+                if (confirm == DialogResult.Yes)
+                {
+                    _damagedService.Delete(txtPlateNumber.Text.Trim());
+                    MessageBox.Show("Data deleted successfully!");
+
+                    LoadDamagedData();
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting data: " + ex.Message);
+            }
+        }
+
+        private void dataGridViewDamaged_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = dataGridViewDamaged.Rows[e.RowIndex];
+                    txtPlateNumber.Text = row.Cells["PlateNumber"].Value.ToString();
+                    comboCarType.Text = row.Cells["CarType"].Value.ToString();
+                    comboServiceType.Text = row.Cells["ServiceType"].Value.ToString();
+                    txtDescription.Text = row.Cells["Description"].Value?.ToString();
+                    dateReport.Value = Convert.ToDateTime(row.Cells["ReportDate"].Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error selecting row: " + ex.Message);
+            }
         }
     }
-}
+    }
+
