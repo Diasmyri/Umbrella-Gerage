d976a12461ee1c6153cfd748ac8a685a907e4808
Diasmyri <diasmayri15@gmail.com>
2025-11-01 12:48:25 +0700

    perubahan

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,3 @@
-using Umbrella_gerage.Forms;
-
 namespace Umbrella_gerage
 {
     public partial class Form1 : Form
@@ -7,44 +5,11 @@ namespace Umbrella_gerage
         public Form1()
         {
             InitializeComponent();
-            InitializePanelContainer();
         }
-        private Panel panelContainer;
-
-        private void InitializePanelContainer()
-        {
-            panelContainer = new Panel
-            {
-                Dock = DockStyle.Fill,
-                BackColor = Color.WhiteSmoke, // biar lebih netral dan terang
-                Visible = true // langsung tampil, karena ini area utama
-            };
-
-            this.Controls.Add(panelContainer);
-            panelContainer.BringToFront();
-        }
-
-        // ? Fungsi untuk menampilkan form di dalam panel
-        private void LoadFormToPanel(Form form)
-        {
-            if (panelContainer == null)
-            {
-                InitializePanelContainer();
-            }
-
-            panelContainer.Controls.Clear(); // hapus form sebelumnya
-            form.TopLevel = false;
-            form.FormBorderStyle = FormBorderStyle.None;
-            form.Dock = DockStyle.Fill;
-            panelContainer.Controls.Add(form);
-            form.Show();
-        }
-
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            LoadFormToPanel(new ClientForm());
+
         }
     }
 }
-    
--- Forms/PaymentForm.Designer.cs
+++ Forms/PaymentForm.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Payment";
+            Label label1;
+            Label label3;
+            Label label4;
+            Label label5;
+            Label label6;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaymentForm));
+            txtAmount = new TextBox();
+            datePayment = new DateTimePicker();
+            comboMethod = new ComboBox();
+            dataGridViewPayment = new DataGridView();
+            comboClientId = new ComboBox();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPayment).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Blacklisted", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.MenuHighlight;
+            label1.Location = new Point(288, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(188, 55);
+            label1.TabIndex = 2;
+            label1.Text = "PAYMENT";
+            label1.UseMnemonic = false;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Coral;
+            label3.Location = new Point(73, 171);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 27);
+            label3.TabIndex = 6;
+            label3.Text = "AMOUNT";
+            label3.UseMnemonic = false;
+            // 
+            // txtAmount
+            // 
+            txtAmount.Location = new Point(350, 171);
+            txtAmount.Margin = new Padding(3, 4, 3, 4);
+            txtAmount.Name = "txtAmount";
+            txtAmount.Size = new Size(300, 27);
+            txtAmount.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Coral;
+            label4.Location = new Point(73, 216);
+            label4.Name = "label4";
+            label4.Size = new Size(252, 27);
+            label4.TabIndex = 7;
+            label4.Text = "PAYMENT DATE";
+            label4.UseMnemonic = false;
+            // 
+            // datePayment
+            // 
+            datePayment.Location = new Point(350, 213);
+            datePayment.Name = "datePayment";
+            datePayment.Size = new Size(300, 27);
+            datePayment.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Coral;
+            label5.Location = new Point(73, 254);
+            label5.Name = "label5";
+            label5.Size = new Size(127, 27);
+            label5.TabIndex = 9;
+            label5.Text = "METHOD";
+            label5.UseMnemonic = false;
+            // 
+            // comboMethod
+            // 
+            comboMethod.FormattingEnabled = true;
+            comboMethod.Items.AddRange(new object[] { "Dana", "Gopay" });
+            comboMethod.Location = new Point(350, 253);
+            comboMethod.Name = "comboMethod";
+            comboMethod.Size = new Size(300, 28);
+            comboMethod.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Coral;
+            label6.Location = new Point(73, 131);
+            label6.Name = "label6";
+            label6.Size = new Size(196, 26);
+            label6.TabIndex = 12;
+            label6.Text = "CLIENT ID";
+            label6.UseMnemonic = false;
+            // 
+            // dataGridViewPayment
+            // 
+            dataGridViewPayment.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewPayment.Location = new Point(350, 287);
+            dataGridViewPayment.Name = "dataGridViewPayment";
+            dataGridViewPayment.RowHeadersWidth = 51;
+            dataGridViewPayment.Size = new Size(300, 163);
+            dataGridViewPayment.TabIndex = 13;
+            // 
+            // comboClientId
+            // 
+            comboClientId.FormattingEnabled = true;
+            comboClientId.Location = new Point(350, 129);
+            comboClientId.Name = "comboClientId";
+            comboClientId.Size = new Size(300, 28);
+            comboClientId.TabIndex = 14;
+            // 
+            // PaymentForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(747, 462);
+            Controls.Add(comboClientId);
+            Controls.Add(dataGridViewPayment);
+            Controls.Add(label6);
+            Controls.Add(comboMethod);
+            Controls.Add(label5);
+            Controls.Add(datePayment);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(txtAmount);
+            Controls.Add(label1);
+            Name = "PaymentForm";
+            Text = "Payment";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPayment).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtPAY;
+        private TextBox txtAmount;
+        private DateTimePicker datePayment;
+        private ComboBox comboMethod;
+        private DataGridView dataGridViewPayment;
+        private ComboBox comboClientId;
+        private TextBox txtClientId;
     }
 }
