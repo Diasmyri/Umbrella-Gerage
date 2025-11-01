78e3abe357837820c05de7067693370aff9d1aba
Farhan-Almaki <Lathifatulalmaki@gmail.com>
2025-11-01 13:16:13 +0700

    update dibagian payment

--- Forms/PaymentForm.Designer.cs
+++ Forms/PaymentForm.Designer.cs
@@ -39,6 +39,9 @@
             comboMethod = new ComboBox();
             dataGridViewPayment = new DataGridView();
             comboClientId = new ComboBox();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
             label1 = new Label();
             label3 = new Label();
             label4 = new Label();
@@ -50,11 +53,11 @@
             // label1
             // 
             label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Blacklisted", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.MenuHighlight;
-            label1.Location = new Point(288, 30);
+            label1.Location = new Point(252, 22);
             label1.Name = "label1";
-            label1.Size = new Size(188, 55);
+            label1.Size = new Size(164, 41);
             label1.TabIndex = 2;
             label1.Text = "PAYMENT";
             label1.UseMnemonic = false;
@@ -62,99 +65,140 @@
             // label3
             // 
             label3.BackColor = Color.Transparent;
-            label3.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.Coral;
-            label3.Location = new Point(73, 171);
+            label3.Location = new Point(64, 128);
             label3.Name = "label3";
-            label3.Size = new Size(136, 27);
+            label3.Size = new Size(119, 20);
             label3.TabIndex = 6;
             label3.Text = "AMOUNT";
             label3.UseMnemonic = false;
             // 
-            // txtAmount
-            // 
-            txtAmount.Location = new Point(350, 171);
-            txtAmount.Margin = new Padding(3, 4, 3, 4);
-            txtAmount.Name = "txtAmount";
-            txtAmount.Size = new Size(300, 27);
-            txtAmount.TabIndex = 5;
-            // 
             // label4
             // 
             label4.BackColor = Color.Transparent;
-            label4.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.Coral;
-            label4.Location = new Point(73, 216);
+            label4.Location = new Point(64, 162);
             label4.Name = "label4";
-            label4.Size = new Size(252, 27);
+            label4.Size = new Size(220, 20);
             label4.TabIndex = 7;
             label4.Text = "PAYMENT DATE";
             label4.UseMnemonic = false;
             // 
-            // datePayment
-            // 
-            datePayment.Location = new Point(350, 213);
-            datePayment.Name = "datePayment";
-            datePayment.Size = new Size(300, 27);
-            datePayment.TabIndex = 8;
-            // 
             // label5
             // 
             label5.BackColor = Color.Transparent;
-            label5.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label5.ForeColor = Color.Coral;
-            label5.Location = new Point(73, 254);
+            label5.Location = new Point(64, 190);
             label5.Name = "label5";
-            label5.Size = new Size(127, 27);
+            label5.Size = new Size(111, 20);
             label5.TabIndex = 9;
             label5.Text = "METHOD";
             label5.UseMnemonic = false;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Coral;
+            label6.Location = new Point(64, 98);
+            label6.Name = "label6";
+            label6.Size = new Size(172, 20);
+            label6.TabIndex = 12;
+            label6.Text = "CLIENT ID";
+            label6.UseMnemonic = false;
+            // 
+            // txtAmount
+            // 
+            txtAmount.Location = new Point(306, 128);
+            txtAmount.Name = "txtAmount";
+            txtAmount.Size = new Size(263, 23);
+            txtAmount.TabIndex = 5;
+            txtAmount.TextChanged += txtAmount_TextChanged;
+            // 
+            // datePayment
+            // 
+            datePayment.Location = new Point(306, 160);
+            datePayment.Margin = new Padding(3, 2, 3, 2);
+            datePayment.Name = "datePayment";
+            datePayment.Size = new Size(263, 23);
+            datePayment.TabIndex = 8;
+            datePayment.ValueChanged += datePayment_ValueChanged;
             // 
             // comboMethod
             // 
             comboMethod.FormattingEnabled = true;
             comboMethod.Items.AddRange(new object[] { "Dana", "Gopay" });
-            comboMethod.Location = new Point(350, 253);
+            comboMethod.Location = new Point(306, 190);
+            comboMethod.Margin = new Padding(3, 2, 3, 2);
             comboMethod.Name = "comboMethod";
-            comboMethod.Size = new Size(300, 28);
+            comboMethod.Size = new Size(263, 23);
             comboMethod.TabIndex = 10;
-            // 
-            // label6
-            // 
-            label6.BackColor = Color.Transparent;
-            label6.Font = new Font("CollegiateInsideFLF", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.ForeColor = Color.Coral;
-            label6.Location = new Point(73, 131);
-            label6.Name = "label6";
-            label6.Size = new Size(196, 26);
-            label6.TabIndex = 12;
-            label6.Text = "CLIENT ID";
-            label6.UseMnemonic = false;
+            comboMethod.SelectedIndexChanged += comboMethod_SelectedIndexChanged;
             // 
             // dataGridViewPayment
             // 
             dataGridViewPayment.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewPayment.Location = new Point(350, 287);
+            dataGridViewPayment.Location = new Point(306, 215);
+            dataGridViewPayment.Margin = new Padding(3, 2, 3, 2);
             dataGridViewPayment.Name = "dataGridViewPayment";
             dataGridViewPayment.RowHeadersWidth = 51;
-            dataGridViewPayment.Size = new Size(300, 163);
+            dataGridViewPayment.Size = new Size(262, 122);
             dataGridViewPayment.TabIndex = 13;
+            dataGridViewPayment.CellContentClick += dataGridViewPayment_CellContentClick;
             // 
             // comboClientId
             // 
             comboClientId.FormattingEnabled = true;
-            comboClientId.Location = new Point(350, 129);
+            comboClientId.Location = new Point(306, 97);
+            comboClientId.Margin = new Padding(3, 2, 3, 2);
             comboClientId.Name = "comboClientId";
-            comboClientId.Size = new Size(300, 28);
+            comboClientId.Size = new Size(263, 23);
             comboClientId.TabIndex = 14;
+            comboClientId.SelectedIndexChanged += comboClientId_SelectedIndexChanged;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(306, 366);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 15;
+            btnAdd.Text = "ADD";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(399, 366);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 16;
+            btnUpdate.Text = "UPDATE";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(493, 366);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 17;
+            btnDelete.Text = "DELETE";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
             // 
             // PaymentForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(747, 462);
+            ClientSize = new Size(641, 407);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
             Controls.Add(comboClientId);
             Controls.Add(dataGridViewPayment);
             Controls.Add(label6);
@@ -165,8 +209,10 @@
             Controls.Add(label3);
             Controls.Add(txtAmount);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "PaymentForm";
             Text = "Payment";
+            Load += PaymentForm_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridViewPayment).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -181,5 +227,8 @@
         private DataGridView dataGridViewPayment;
         private ComboBox comboClientId;
         private TextBox txtClientId;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
     }
 }
--- Forms/PaymentForm.cs
+++ Forms/PaymentForm.cs
@@ -1,20 +1,217 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Umbrella_gerage.Data;
+using Umbrella_gerage.Models;
 
 namespace Umbrella_gerage.Forms
 {
-    public partial class PaymentForm: Form
+    public partial class PaymentForm : Form
     {
+        private readonly AppDbContext _context;
+
         public PaymentForm()
         {
             InitializeComponent();
+            _context = new AppDbContext();
+        }
+
+        // ===================== FORM LOAD =====================
+        private void PaymentForm_Load(object sender, EventArgs e)
+        {
+            LoadClients();
+            LoadPayments();
+            comboMethod.Items.Clear();
+            comboMethod.Items.Add("Dana");
+            comboMethod.Items.Add("Gopay");
+        }
+
+        // ===================== LOAD DATA =====================
+        private void LoadClients()
+        {
+            try
+            {
+                comboClientId.DataSource = _context.Clients.ToList();
+                comboClientId.DisplayMember = "ClientId";   // tampilkan ID
+                comboClientId.ValueMember = "ClientId";
+                comboClientId.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading clients: " + ex.Message);
+            }
+        }
+
+        private void LoadPayments()
+        {
+            try
+            {
+                var payments = _context.Payments
+                    .Select(p => new
+                    {
+                        p.PaymentId,
+                        p.ClientId,
+                        p.Amount,
+                        p.PaymentDate,
+                        p.Method
+                    })
+                    .ToList();
+
+                dataGridViewPayment.DataSource = payments;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading payments: " + ex.Message);
+            }
+        }
+
+        private void ClearForm()
+        {
+            comboClientId.SelectedIndex = -1;
+            txtAmount.Clear();
+            comboMethod.SelectedIndex = -1;
+            datePayment.Value = DateTime.Now;
+        }
+
+        // ===================== EVENT HANDLERS =====================
+        private void comboClientId_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Kosongkan (tidak wajib diisi)
+        }
+
+        private void txtAmount_TextChanged(object sender, EventArgs e)
+        {
+            if (!decimal.TryParse(txtAmount.Text, out _) && txtAmount.Text != "")
+            {
+                MessageBox.Show("Please enter a valid amount.", "Invalid Input");
+                txtAmount.Clear();
+            }
+        }
+
+        private void datePayment_ValueChanged(object sender, EventArgs e)
+        {
+            // Tidak perlu isi apa-apa
+        }
+
+        private void comboMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Tidak perlu isi apa-apa
+        }
+
+        private void dataGridViewPayment_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = dataGridViewPayment.Rows[e.RowIndex];
+                    comboClientId.SelectedValue = row.Cells["ClientId"].Value;
+                    txtAmount.Text = row.Cells["Amount"].Value.ToString();
+                    datePayment.Value = Convert.ToDateTime(row.Cells["PaymentDate"].Value);
+                    comboMethod.Text = row.Cells["Method"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error selecting row: " + ex.Message);
+            }
+        }
+
+        // ===================== BUTTON ACTIONS =====================
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboClientId.SelectedValue == null ||
+                    string.IsNullOrEmpty(txtAmount.Text) ||
+                    comboMethod.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Please fill all fields.");
+                    return;
+                }
+
+                var payment = new Payment
+                {
+                    ClientId = (int)comboClientId.SelectedValue,
+                    Amount = decimal.Parse(txtAmount.Text),
+                    PaymentDate = datePayment.Value,
+                    Method = comboMethod.Text
+                };
+
+                _context.Payments.Add(payment);
+                _context.SaveChanges();
+
+                MessageBox.Show("Payment added successfully!");
+                LoadPayments();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding payment: " + ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridViewPayment.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Please select a payment to update.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dataGridViewPayment.SelectedRows[0].Cells["PaymentId"].Value);
+                var payment = _context.Payments.FirstOrDefault(p => p.PaymentId == id);
+
+                if (payment != null)
+                {
+                    payment.ClientId = (int)comboClientId.SelectedValue;
+                    payment.Amount = decimal.Parse(txtAmount.Text);
+                    payment.PaymentDate = datePayment.Value;
+                    payment.Method = comboMethod.Text;
+
+                    _context.SaveChanges();
+
+                    MessageBox.Show("Payment updated successfully!");
+                    LoadPayments();
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating payment: " + ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridViewPayment.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Please select a payment to delete.");
+                    return;
+                }
+
+                int id = Convert.ToInt32(dataGridViewPayment.SelectedRows[0].Cells["PaymentId"].Value);
+                var payment = _context.Payments.FirstOrDefault(p => p.PaymentId == id);
+
+                if (payment != null)
+                {
+                    _context.Payments.Remove(payment);
+                    _context.SaveChanges();
+
+                    MessageBox.Show("Payment deleted successfully!");
+                    LoadPayments();
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting payment: " + ex.Message);
+            }
         }
     }
 }
