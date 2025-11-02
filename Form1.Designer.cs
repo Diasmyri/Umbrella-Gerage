ef82714d674a45ac0a79a2e4254ac8b4fff4bffb
Diasmyri <diasmayri15@gmail.com>
2025-11-02 13:51:46 +0700

    edit form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,47 +2,75 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem clientToolStripMenuItem;
+        private ToolStripMenuItem damagedToolStripMenuItem;
+        private ToolStripMenuItem paymentToolStripMenuItem;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            if (disposing && components != null)
             {
                 components.Dispose();
             }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            clientToolStripMenuItem = new ToolStripMenuItem();
+            damagedToolStripMenuItem = new ToolStripMenuItem();
+            paymentToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { clientToolStripMenuItem, damagedToolStripMenuItem, paymentToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(951, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // clientToolStripMenuItem
+            // 
+            clientToolStripMenuItem.Name = "clientToolStripMenuItem";
+            clientToolStripMenuItem.Size = new Size(50, 20);
+            clientToolStripMenuItem.Text = "Client";
+            clientToolStripMenuItem.Click += clientToolStripMenuItem_Click;
+            // 
+            // damagedToolStripMenuItem
+            // 
+            damagedToolStripMenuItem.Name = "damagedToolStripMenuItem";
+            damagedToolStripMenuItem.Size = new Size(70, 20);
+            damagedToolStripMenuItem.Text = "Damaged";
+            damagedToolStripMenuItem.Click += damagedToolStripMenuItem_Click;
+            // 
+            // paymentToolStripMenuItem
+            // 
+            paymentToolStripMenuItem.Name = "paymentToolStripMenuItem";
+            paymentToolStripMenuItem.Size = new Size(66, 20);
+            paymentToolStripMenuItem.Text = "Payment";
+            paymentToolStripMenuItem.Click += paymentToolStripMenuItem_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(951, 469);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Umbrella Garage System";
             Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
-
-        #endregion
-
-        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,4 @@
 using Umbrella_gerage.Forms;
-using Umbrella_gerage.Models;
 
 namespace Umbrella_gerage
 {
@@ -8,16 +7,40 @@ namespace Umbrella_gerage
         public Form1()
         {
             InitializeComponent();
+            this.IsMdiContainer = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void OpenChildForm(Form childForm)
         {
-            
+            // Tutup semua form anak sebelumnya
+            foreach (Form form in this.MdiChildren)
+            {
+                form.Close();
+            }
+
+            // Tampilkan form baru dalam container
+            childForm.MdiParent = this;
+            childForm.WindowState = FormWindowState.Maximized;
+            childForm.Show();
+        }
+
+        private void clientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new ClientForm());
+        }
+
+        private void damagedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new DamagedForm());
+        }
+
+        private void paymentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new PaymentForm());
         }
     }
 }
